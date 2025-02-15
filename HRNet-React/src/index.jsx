8983f0f194ef5024575086f8660b538b19fd8db1
Krehion <krehion@hotmail.com>
2025-02-15 14:55:53 +0100

    :bug: fix wrong file name causing error in routing

--- HRNet-React/src/index.jsx
+++ HRNet-React/src/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/style.scss";
 import AppRouter from "./components/router/AppRouter.jsx";
-import { UserProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<UserProvider>
-			<AppRouter />
-		</UserProvider>
+		<AppRouter />
 	</React.StrictMode>
 );

7aafb783c44f8af242e3e58c0fe37c7df03d482d
Krehion <krehion@hotmail.com>
2025-02-16 17:49:18 +0100

    :sparkles: add employee list page + basic style

--- HRNet-React/src/pages/employeeList/EmployeeList.jsx
+++ HRNet-React/src/pages/employeeList/EmployeeList.jsx
@@ -1,9 +1,15 @@
-const EmployeeList = () => {
+import { Link } from "react-router-dom";
+
+import "../../style/layout/_employeelist.scss";
+
+export default function CreateEmployee() {
 	return (
-		<div>
-			<p>Prout</p>
+		<div className="global-container list-bkgd">
+			<div className="list-container">
+				<h1>Current employees</h1>
+				{/* List */}
+				<Link to="/">Home</Link>
+			</div>
 		</div>
 	);
-};
-
-export default EmployeeList;
+}

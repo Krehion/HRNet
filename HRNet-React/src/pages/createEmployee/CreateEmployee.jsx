84800b9f8d94535fabfaf099459eac5deaef8ce0
Krehion <krehion@hotmail.com>
2025-02-16 15:35:24 +0100

    :lipstick: add form container + style

--- HRNet-React/src/pages/createEmployee/CreateEmployee.jsx
+++ HRNet-React/src/pages/createEmployee/CreateEmployee.jsx
@@ -1,9 +1,20 @@
-const CreateEmployee = () => {
+import { Link } from "react-router-dom";
+
+import "../../style/layout/_createemployee.scss";
+
+export default function CreateEmployee() {
 	return (
-		<div>
-			<p>Prout</p>
+		<div className="global-container accomodation-bkgd">
+			<div className="form-container">
+				<div className="logo-container">
+					<img src="/src/assets/logo.png" className="logo" />
+					<h1>HRNet</h1>
+				</div>
+				<div role="separator" className="separator">
+					<Link to="employee-list">View current employees</Link>
+				</div>
+				<h2>Create employee</h2>
+			</div>
 		</div>
 	);
-};
-
-export default CreateEmployee;
+}

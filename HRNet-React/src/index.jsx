4a83029b6b91a3ffe75d1d85e9dc8cb0897a8697
Krehion <krehion@hotmail.com>
2025-02-24 18:56:32 +0100

    :truck: move router into utils folder

--- HRNet-React/src/index.jsx
+++ HRNet-React/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./components/router/AppRouter.jsx";
+import AppRouter from "./utils/router/AppRouter.jsx";
 import "./style/style.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 

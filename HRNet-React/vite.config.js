95756fe25b3f4c345b257d0eb81b4dbd0697fbe9
Krehion <krehion@hotmail.com>
2025-02-25 20:38:15 +0100

    :zap: perf: enable text compression with vite-plugin-compression

--- HRNet-React/vite.config.js
+++ HRNet-React/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		compression({
+			verbose: true,
+			algorithm: "gzip", // 'brotli' for Brotli compression
+			threshold: 10240, // The minimum file size (in bytes) for compression to kick in
+			deleteOriginFile: false // Keep original files
+		})
+	]
+});
